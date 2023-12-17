59540c85242dca52abe748f30b7d810eb0339ea6
Suprim Nakarmi <suprimnakarmi@Suprim-Nakarmi.local>
2023-12-17 16:08:22 -0600

    updated evaluation.py file in steps to measure the r2, mse, rmse, and added src/evaluation.py file to predict and calculate metrics

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -2,6 +2,32 @@ import logging
 from zenml import step
 import pandas as pd
 
+from typing import Tuple
+from sklearn.base import RegressorMixin
+from typing_extensions import Annotated
+from zenml import step
+
+from src.evaluation import MSE, R2, RMSE
+
 @step
-def evaluate_model(df: pd.DataFrame) -> None:
-    pass
+def evaluate_model(model: RegressorMixin,
+                   X_test: pd.DataFrame,
+                   y_test: pd.DataFrame,) -> Tuple[
+                       Annotated[float, "r2_score"],
+                       Annotated[float, "rmse"],
+                   ]:
+    try:
+        prediction = model.predict(X_test)
+        mse_class = MSE()
+        mse = mse_class.calculate_scores(y_test, prediction)
+
+        r2_class = R2()
+        r2 = r2_class.calculate_scores(y_test, prediction)
+
+        rmse_class = RMSE()
+        rmse = rmse_class.calculate_scores(y_test, prediction)
+
+        return r2, rmse
+    except Exception as e:
+        logging.error("Error in evaluating model: {}".format(e))
+        raise e
