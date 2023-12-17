5f2f4140299faccd4f994d4000e646b570903f3a
Suprim Nakarmi <suprimnakarmi@Suprim-Nakarmi.local>
2023-12-16 18:36:34 -0600

    created data_cleaning.py file that consists of data cleaning and data splitting (train/test) code

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -4,7 +4,7 @@ from steps.clean_data import clean_df
 from steps.model_train import train_model
 from steps.evaluation import evaluate_model
 
-@pipeline
+@pipeline(enable_cache=False)
 def train_pipeline(data_path:str):
     df = ingest_df(data_path)
     clean_df(df)
