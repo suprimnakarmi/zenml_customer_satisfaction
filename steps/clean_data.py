bba9a5b91bba6e22f5a2ec7067775e6eef447497
Suprim Nakarmi <suprimnakarmi@Suprim-Nakarmi.local>
2023-12-16 18:51:02 -0600

    updated the clean_data.py by adding code to clean and split the data into training and test set

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,8 +1,30 @@
 import logging
 import pandas as pd
 from zenml import step
+from typing_extensions import Annotated
+from typing import Tuple
+from zenml import step
+from src.data_cleaning import DataCleaning, DataDivideStrategy, DataPreProcessStrategy
+
 
 
 @step
-def clean_df(df: pd.DataFrame) -> None:
-    pass
+def clean_df(df: pd.DataFrame) -> Tuple [
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series,"y_test"],
+]:
+    try:
+        process_strategy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(df, process_strategy)
+        processed_data = data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data cleaning completed")
+
+    except Exception as e:
+        logging.error("Error in cleaning data: {}".format(e))
+        raise e
